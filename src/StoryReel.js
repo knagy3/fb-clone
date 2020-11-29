fede7eb9609e04eaafbc0adcb929e9f8fccbeaba
Kristof Richard Nagy <knagy3@jaguarlandrover.com>
2020-11-29 11:13:18 +0100

    is deployed

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -6,29 +6,29 @@ function StoryReel() {
     return (
         <div className="storyReel">
             <Story
-                image='https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/119733779/original/cc2e5944473be8570911a5f558f0d28a150e3cdc/paint-you-a-custom-abstract-portrait-drawing-illustration.png'
-                profileSrc='https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4'
-                title='Sonny Sangha'
+                image='https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/51461740_2204775919580350_5488419689719660544_o.jpg?_nc_cat=101&ccb=2&_nc_sid=8bfeb9&_nc_ohc=9-3yQFZ2ygMAX8Ob--b&_nc_ht=scontent-vie1-1.xx&oh=77634472ad8f3ca14e8b778467c12bbd&oe=5FEA1E15'
+                profileSrc='https://lh5.googleusercontent.com/-ZAy0Q1TRALU/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuckqBMiyHlW1NXKcIVPwsFclC4rftQ/s96-c/photo.jpg'
+                title='Kristof Nagy'
             />
             <Story
-                image='https://assets.yellowtrace.com.au/wp-content/uploads/2019/07/25141848/Abstract-Portrait-Paintings-by-Joseph-Lee-Yellowtrace-05.jpg'
-                profileSrc='https://media-exp1.licdn.com/dms/image/C4E03AQGivOvOWtHbhQ/profile-displayphoto-shrink_200_200/0?e=1602115200&v=beta&t=KLHRHD6oZcHMHCmlVEqS7wGEEnjiYn9HdUzt41SNrcY'
-                title='Rafeh Qazi '
+                image='https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/117304166_3360287654029165_2181844251015719478_o.jpg?_nc_cat=109&ccb=2&_nc_sid=8bfeb9&_nc_ohc=_nZ_ONAVCBkAX-nuCQl&_nc_ht=scontent-vie1-1.xx&oh=f1406f5dce43e1349532f440977f92ac&oe=5FEA68BB'
+                profileSrc='https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/13119067_1111593505565269_2672530350444250907_n.jpg?_nc_cat=109&ccb=2&_nc_sid=09cbfe&_nc_ohc=t11KgOea45oAX8HVA_u&_nc_ht=scontent-vie1-1.xx&oh=1006bde141ba3e57d4bd20a288c638f9&oe=5FE8BA20'
+                title='Laszlo Csizmadia'
             />
             <Story
-                image='https://cdn.shopify.com/s/files/1/0128/3672/products/DIMENSIONAL_ABSTRACT_PORTRAIT_no._IV_1024x1024.png?v=1507719723'
-                profileSrc='https://miro.medium.com/fit/c/336/336/2*4lH0jftaq_sPRqQisUsVqw.jpeg'
-                title='Nazariy Dumanskyy '
+                image='https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/121201568_3942973842385731_2564543595291025522_o.jpg?_nc_cat=104&ccb=2&_nc_sid=84a396&_nc_ohc=TtEBHJn73SEAX9GP6Bk&_nc_ht=scontent-vie1-1.xx&oh=07261ed804001c1fe0b61c4f25d061a9&oe=5FE95857'
+                profileSrc='https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/69159_168616173154869_7984235_n.jpg?_nc_cat=107&ccb=2&_nc_sid=09cbfe&_nc_ohc=Tz0xy3W1oPYAX-m3Iw_&_nc_ht=scontent-vie1-1.xx&oh=720f622c0ef490fe1c5fa67d44879994&oe=5FE9B80E'
+                title='Laszlo Nagy'
             />
             <Story
-                image='https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/119733779/original/cc2e5944473be8570911a5f558f0d28a150e3cdc/paint-you-a-custom-abstract-portrait-drawing-illustration.png'
-                profileSrc='https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4'
-                title='Sonny Sangha'
+                image='https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/68661705_2826088140741813_24019578833076224_o.jpg?_nc_cat=110&ccb=2&_nc_sid=8bfeb9&_nc_ohc=dbuvwSMEITcAX-8_zWM&_nc_ht=scontent-vie1-1.xx&oh=bda2855cbdc72f10a8bb93493b559991&oe=5FE771EC'
+                profileSrc='https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/58796889_2399350253444081_5233791763302318080_o.jpg?_nc_cat=111&ccb=2&_nc_sid=09cbfe&_nc_ohc=gyHjeQWhiwgAX_6eOTj&_nc_ht=scontent-vie1-1.xx&oh=36aafa3a1fb882029dd5e803deb03f83&oe=5FE85A2B'
+                title='Balint Nagy'
             />
             <Story
-                image='https://cdn.shopify.com/s/files/1/0128/3672/products/DIMENSIONAL_ABSTRACT_PORTRAIT_no._IV_1024x1024.png?v=1507719723'
-                profileSrc='https://miro.medium.com/fit/c/336/336/2*4lH0jftaq_sPRqQisUsVqw.jpeg'
-                title='Nazariy Dumanskyy '
+                image='https://scontent-vie1-1.xx.fbcdn.net/v/t31.0-8/20615788_1632293416782922_5607627054743280745_o.jpg?_nc_cat=105&ccb=2&_nc_sid=19026a&_nc_ohc=i0l9eujbTWEAX-e_1LH&_nc_ht=scontent-vie1-1.xx&oh=49a58a949104d9723a97e2fd3dff0f14&oe=5FE73B00'
+                profileSrc='https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/58419984_2444065438939045_1880425815645093888_o.jpg?_nc_cat=110&ccb=2&_nc_sid=09cbfe&_nc_ohc=_ZXRCRG6ALcAX8pIEZt&_nc_ht=scontent-vie1-1.xx&oh=80ae188bdc03f9cd0f5e5cca3f6722fa&oe=5FE7A7C5'
+                title='Daniella Nagy'
             />
         </div>
     )
