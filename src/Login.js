a6472c68cd3492a0506b0a24d2aa4d11979f9fdf
knagy3 <knagy3@jaguarlandrover.com>
2021-07-05 22:39:27 +0200

    icon is changed

--- src/Login.js
+++ src/Login.js
@@ -17,14 +17,13 @@ function Login() {
                 type: actionTypes.SET_USER,
                 user: result.user,
             });
-            console.log(result);
         }).catch((error) => alert(error.message));
     };
 
     return (
         <div className='login' >
             <div className="login__logo">
-                <img src="https://www.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png" alt="fb circle" />
+                <img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Messenger_colored_svg-256.png" alt="fb circle" />
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Facebook_Logo_%282019%29.svg/1280px-Facebook_Logo_%282019%29.svg.png" alt="fb text"/>
             </div>
             <Button type='submit' onClick={signIn}>
